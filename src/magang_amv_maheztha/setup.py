25ba23f31832a8d8b7b3f60fb4d334001ff2c9ad
Camn0 <messusanssi@gmail.com>
2026-02-11 00:01:27 +0700

    fixing package name

--- src/magang_amv_maheztha/setup.py
+++ src/magang_amv_maheztha/setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-package_name = 'magang_amv'
+package_name = 'magang_amv_maheztha'
 
 setup(
     name=package_name,
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'publisher = magang_amv_maheztha.node:main',
         ],
     },
 )
